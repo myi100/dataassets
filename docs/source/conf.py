1f868fa5008b83d97bc82a4f09935ca1f88c59e8
Muhammed Y. Idris <myi100@users.noreply.github.com>
2023-03-16 13:25:22 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MSM CRC'
+copyright = '2023'
+author = 'MYI'
 
 release = '0.1'
 version = '0.1.0'
